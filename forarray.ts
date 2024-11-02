b28869d31d69e6f5345e418312f26e8771264a4f
bob@example.com <bob@example.com>
2024-11-02 13:36:43 +0300

    create: sheep.js

---
+++ forarray.ts
@@ -0,0 +1,8 @@
+const fruit = ["grusha", "yabloko", "persik", "ananas", "abrikos"]
+
+for (let x=0; x<fruit.length; x++){
+    if (fruit[x]==="yabloko"){
+        console.log(`в данном массиве под номером ${x+1} есть фрукт ${fruit[x]}`)
+    }
+}
+
