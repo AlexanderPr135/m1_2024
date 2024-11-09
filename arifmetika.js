5e59668d0494a972599e98ae1a3ff61741027cac
bob@example.com <bob@example.com>
2024-11-09 13:10:39 +0300

    create: arifmetika.js

---
+++ arifmetika.js
@@ -0,0 +1,9 @@
+function plus(...params){
+    let result=1
+    for (i of params){
+        result=result*i
+
+    }
+    return result
+}
+console.log(plus(1,2,3,4,5,6,7,8,9,10,11,12,13,14654,4658578578576,100000000))
