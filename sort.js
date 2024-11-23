b0b3ab1b7f79785c0308082253bd523d313331b8
bob@example.com <bob@example.com>
2024-11-23 13:23:44 +0300

    edit: sort.js

---
+++ sort.js
@@ -0,0 +1,5 @@
+const randommassive = ['yabloko', 'ananas', 'frukty', 'mashina', 'chelovek']
+const resultUp = randommassive.sort();
+console.log(resultUp);
+const resultDown = randommassive.reverse();
+console.log(resultDown);
